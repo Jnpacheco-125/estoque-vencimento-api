38e5ba4fb9ca0d56a36d35a20d1234fb88a64d3b
Jnpacheco-125 <rm350127@fiap.com.br>
2025-09-16 13:19:33 -0300

    Initial commit

---
+++ src/main/java/me/dio/EstoqueVencimentoApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueVencimentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueVencimentoApiApplication.class, args);
+	}
+
+}
